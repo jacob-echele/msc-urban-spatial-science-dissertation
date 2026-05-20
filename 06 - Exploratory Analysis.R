c2fcce9796f038e0e2bd396cc06bac06e6812e42
jacob-echele <ucfnjte@ucl.ac.uk>
2026-05-20 16:41:55 +0100

    Combined correlation matrix added

--- 06 - Exploratory Analysis.R
+++ 06 - Exploratory Analysis.R
@@ -7,6 +7,8 @@ library(readr)
 library(stringr)
 library(RColorBrewer)
 library(tidyverse)
+library(corrplot) #for correlation matrix
+library(Hmisc) #for correlation matrix
 
 ###########################
 #   EXPLORATORY ANALYSIS
@@ -293,3 +295,176 @@ ggsave(
   height = 5,
   dpi = 300
 )
+
+##################################
+#   COMBINED CORRELATION MATRIX
+##################################
+
+#ensure hex_id data type matches
+stl_hex_fp$hex_id <- as.character(stl_hex_fp$hex_id)
+stl_hex_population_density$hex_id <- as.character(stl_hex_population_density$hex_id)
+stl_hex_job_density$hex_id <- as.character(stl_hex_job_density$hex_id)
+stl_hex_transit_accessibility$hex_id <- as.character(stl_hex_transit_accessibility$hex_id)
+
+#join prepared St. Louis variables into one df
+stl_hex_analysis <- stl_hex_fp%>%
+  st_drop_geometry()%>%
+  select(hex_id, fiscal_productivity)%>%
+  left_join(
+    stl_hex_population_density%>%
+      st_drop_geometry()%>%
+      select(hex_id, pop_density_acre),
+    by = "hex_id"
+  )%>%
+  left_join(
+    stl_hex_job_density%>%
+      st_drop_geometry()%>%
+      select(hex_id, job_density_acre, perceived_job_density),
+    by = "hex_id"
+  )%>%
+  left_join(
+    stl_hex_transit_accessibility%>%
+      st_drop_geometry()%>%
+      select(hex_id, transit_access_jobs_30, transit_access_jobs_45, transit_access_jobs_60),
+    by = "hex_id"
+  )%>%
+  mutate(city = "St. Louis")
+
+#ensure hex_id data type matches
+hennepin_county_hex_fp$hex_id <- as.character(hennepin_county_hex_fp$hex_id)
+hennepin_county_hex_population_density$hex_id <- as.character(hennepin_county_hex_population_density$hex_id)
+hennepin_county_hex_job_density$hex_id <- as.character(hennepin_county_hex_job_density$hex_id)
+hennepin_county_hex_transit_accessibility$hex_id <- as.character(hennepin_county_hex_transit_accessibility$hex_id)
+
+#join prepared Minneapolis variables into one df
+hennepin_county_hex_analysis <- hennepin_county_hex_fp%>%
+  st_drop_geometry()%>%
+  select(hex_id, fiscal_productivity)%>%
+  left_join(
+    hennepin_county_hex_population_density%>%
+      st_drop_geometry()%>%
+      select(hex_id, pop_density_acre),
+    by = "hex_id"
+  )%>%
+  left_join(
+    hennepin_county_hex_job_density%>%
+      st_drop_geometry()%>%
+      select(hex_id, perceived_job_density),
+    by = "hex_id"
+  )%>%
+  left_join(
+    hennepin_county_hex_transit_accessibility%>%
+      st_drop_geometry()%>%
+      select(hex_id, transit_access_jobs_45),
+    by = "hex_id"
+  )%>%
+  mutate(city = "Minneapolis")
+
+#ensure hex_id data type matches
+nyc_hex_fp$hex_id <- as.character(nyc_hex_fp$hex_id)
+nyc_hex_population_density$hex_id <- as.character(nyc_hex_population_density$hex_id)
+nyc_hex_job_density$hex_id <- as.character(nyc_hex_job_density$hex_id)
+nyc_hex_transit_accessibility$hex_id <- as.character(nyc_hex_transit_accessibility$hex_id)
+
+#join new york variables into one dataset
+nyc_hex_analysis <- nyc_hex_fp%>%
+  st_drop_geometry()%>%
+  select(hex_id, fiscal_productivity)%>%
+  left_join(
+    nyc_hex_population_density%>%
+      st_drop_geometry()%>%
+      select(hex_id, pop_density_acre),
+    by = "hex_id"
+  )%>%
+  left_join(
+    nyc_hex_job_density%>%
+      st_drop_geometry()%>%
+      select(hex_id, perceived_job_density),
+    by = "hex_id"
+  )%>%
+  left_join(
+    nyc_hex_transit_accessibility%>%
+      st_drop_geometry()%>%
+      select(hex_id, transit_access_jobs_45),
+    by = "hex_id"
+  )%>%
+  mutate(city = "New York")
+
+# -----------------------
+#    combine variables
+# -----------------------
+
+correlation_data <- bind_rows(
+    stl_hex_analysis%>%
+      st_drop_geometry()%>%
+      mutate(city="St. Louis"),
+    hennepin_county_hex_analysis%>%
+      st_drop_geometry()%>%
+      mutate(city="Minneapolis"),
+    nyc_hex_analysis%>%
+      st_drop_geometry()%>%
+      mutate(city="New York")
+    )
+
+#keep only variables needed for correlation matrix
+correlation_variables <- correlation_data%>%
+  select(fiscal_productivity, pop_density_acre, perceived_job_density, transit_access_jobs_45)%>%
+  filter(
+    fiscal_productivity > 0,
+    pop_density_acre > 0,
+    perceived_job_density > 0,
+    transit_access_jobs_45 > 0
+  )
+
+#log transform variables because values are highly skewed
+correlation_variables_log <- correlation_variables%>%
+  mutate(
+    log_fiscal_productivity = log10(fiscal_productivity),
+    log_pop_density_acre = log10(pop_density_acre),
+    log_perceived_job_density = log10(perceived_job_density),
+    log_transit_access_jobs_45 = log10(transit_access_jobs_45)
+  )%>%
+  select(log_fiscal_productivity, log_pop_density_acre, log_perceived_job_density, log_transit_access_jobs_45)
+
+#calculate correlation matrix
+correlation_matrix <- cor(
+  correlation_variables_log,
+  use = "pairwise.complete.obs",
+  method = "pearson"
+)
+
+#rename rows and columns for cleaner figure
+colnames(correlation_matrix) <- c("Fiscal Productivity", "Population Density", "Perceived Job Density", "Transit Accessibility")
+
+rownames(correlation_matrix) <- c("Fiscal Productivity", "Population Density", "Perceived Job Density", "Transit Accessibility")
+
+correlation_matrix
+
+# ---------------------
+#    actual plotting
+# ---------------------
+
+#combined correlation matrix plot
+corrplot(
+  correlation_matrix,
+  method = "circle", #size of circle depends on strength of correlation
+  type = "lower", #only include lower triangle of results for clarity
+  addCoef.col = "black",
+  number.cex = .8,
+  tl.col = "black",
+  tl.srt = 45, #rotate labels 45 degrees for legibility
+  diag = FALSE,
+  col = colorRampPalette(
+    c(
+      "#ca0020",
+      "white",
+      "#0571b0"
+    )
+  )(200) #sets number of colors in between max (blue) and min (red) values
+)
+
+#########################################
+#   CITY-SPECIFIC CORRELATION MATRICES
+#########################################
+
+#St. Louis
